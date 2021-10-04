10d3fb894509d3bbc3f4782658bcd63d9617d177
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-10-05 04:17:34 +0530

    Feeing so great finally all done

--- hypo.js
+++ hypo.js
@@ -8,10 +8,10 @@ function calculating(a, b){
 }
 
 function squares (){
- var thesum = calculating(Number(sides[0].value), Number(sides[1].value));
- var length = Math.sqrt(thesum).toFixed(2);
+ var finalAns = calculating(Number(sides[0].value), Number(sides[1].value));
+ var length = Math.sqrt(finalAns).toFixed(2);
 
-output.innerText="The length of hypotenuse is " + length + "unites"
+output.innerText="The length of hypotenuse is " + length + " unites"
 }
 
 btn.addEventListener("click", squares);
