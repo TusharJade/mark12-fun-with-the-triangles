325586146ee750ebf417f3c78e25c06578d22ad3
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-10-04 23:46:04 +0530

    Working on quiz logic

---
+++ quiz.js
@@ -0,0 +1,13 @@
+var quizForm =  document.querySelector(".quiz-form");
+var btn = document.querySelector("#btn");
+var output = document.querySelector("#output");
+
+
+
+
+
+
+
+
+
+btn.addEventListener("click", calclulateScore)
